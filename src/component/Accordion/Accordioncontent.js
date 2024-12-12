618ecef42bf4f0384c50e7ed9c2fef5d4916e915
saini-baba <anirudh.saini.smartdata@gmail.com>
2024-12-12 17:56:43 +0530

    update done

--- src/component/Accordion/Accordioncontent.js
+++ src/component/Accordion/Accordioncontent.js
@@ -9,26 +9,26 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import { link } from "../../data/Links";
 export const Accordioncontent = () => {
     const data = [
-        {
-            heading: "Who’s behind Circle Digital Solutions?",
-            para: "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
-        },
-        {
-            heading: "Why not hire a full-time designer and developer",
-            para: "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
-        },
-        {
-            heading: "What if I'm not happy with the results?",
-            para: "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
-        },
-        {
-            heading: "How quickly will I receive my project?",
-            para: "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
-        },
-        {
-            heading: "Can I get a refund?",
-            para: "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
-        },
+      {
+        heading: "Who's behind Circle Digital Solutions?",
+        para: "Circle Digital Solutions was founded by a group of college friends, each bringing expertise in diverse fields such as marketing, designing, and development. Together, we combined our skills and shared passion for innovation to create a digital solutions company that drives results and delivers value to our clients.",
+      },
+      {
+        heading: "Why not hire a full-time designer and developer?",
+        para: "Startups often face fluctuating workloads, and hiring full-time employees can significantly increase operational costs, including salaries, benefits, and overheads. This added expense can make project fees higher, which is not always ideal for clients. By working with Circle Digital Solutions, you gain access to a team of skilled professionals only when you need them, ensuring quality results without the financial burden of full-time staff.",
+      },
+      {
+        heading: "What if I'm not happy with the result?",
+        para: "At Circle Digital Solutions, client satisfaction is our top priority. While such instances are rare, if you’re not completely satisfied with the result, we will work closely with you to understand your concerns and make the necessary adjustments. Our goal is to ensure the final outcome meets your expectations and leaves you completely satisfied with our work.",
+      },
+      {
+        heading: "How quickly will I receive my project?",
+        para: "The timeline for delivering your project depends on its complexity and your specific requirements. However, most projects are typically completed within 10–15 days. We prioritize efficiency without compromising on quality and will keep you informed throughout the process.",
+      },
+      {
+        heading: "Can I get a refund?",
+        para: "We do not offer refunds. Our refund policies will be clearly outlined in the contract to ensure transparency and mutual understanding before the project begins. We are committed to delivering high-quality work and addressing any concerns to meet your expectations.",
+      },
     ];
 
     const [expanded, setExpanded] = React.useState(false);
